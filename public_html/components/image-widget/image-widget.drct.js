b5c6cce03dae01836ff813f26ae9e29ceafe1952
d <dhenton@localhost.com>
2015-10-22 21:14:36 -0500

    image buttons

--- public_html/components/image-widget/image-widget.drct.js
+++ public_html/components/image-widget/image-widget.drct.js
@@ -1,22 +1,24 @@
 angular.module('app').directive('imageWidget', function ($log) {
 
     return {
-        templateUrl: 'components/image-widget/image-widget.tpl.html',
+         
         restrict: 'E',
         scope: {"image": '='},
         compile: function(element,attributes)
         {
             
-            //do one time only stuff here
-           // element.css("border", "1px solid #cccccc");
+          
             
             var linkFunction = function($scope, element, attributes) {
                 
-                var text = "<a href='https://www.pinterest.com/pin/99360735500167749' data-pin-do='embedPin'></a>"
+                var text = "<a href='https://www.pinterest.com/pin/"+
+                        
+                        $scope.image.id
+                        
+                        +"' data-pin-do='embedPin'></a>"
                 
                 element.html(text); 
-                        //+ $scope.image.id);
-               // element.css("background-color", "#ffff00");
+            
             }
 
             return linkFunction;
--- public_html/sections/browse-images/browse-images.ctl.js
+++ public_html/sections/browse-images/browse-images.ctl.js
@@ -2,6 +2,39 @@ angular.module('app').controller('BrowseImagesController', function ($log, image
 
     var vm = this;
     vm.images = images.data;
-    
+    vm.maxImages = 4;
+    vm.imageBuffer = new Array(vm.maxImages);
+    vm.currentPage = 1;
+    vm.totalImages = images.data.length;
+    // console.log(images.length)
 
+    vm.pageChanged = function ()
+    {
+        
+         
+        for (var i = 0; i < vm.maxImages; i++)
+        {
+            vm.imageBuffer[i] = null;
+        }
+        var maxLoop = 0;
+        if (vm.totalImages < vm.maxImages)
+        {
+            maxLoop = vm.totalImages;
+        }
+        else
+        {
+            maxLoop = vm.maxImages;
+        }
+        var start = (vm.currentPage - 1) * vm.maxImages;
+        
+        for (var i = start; i < start + maxLoop; i++)
+        {
+            if (i < vm.images.length)
+            {
+                vm.imageBuffer[i-start] = vm.images[i];
+            }
+        }
+
+    }
+    vm.pageChanged();
 });
