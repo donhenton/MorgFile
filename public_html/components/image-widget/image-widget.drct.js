3475561203386e9601aa5c0e53ffa57819c412f6
d <dhenton@localhost.com>
2015-10-23 11:59:38 -0500

    pin paging working

--- public_html/components/image-widget/image-widget.drct.js
+++ public_html/components/image-widget/image-widget.drct.js
@@ -1,29 +1,35 @@
-angular.module('app').directive('imageWidget', function ($log) {
+angular.module('app').directive('imageWidget', function ($log,$timeout) {
 
     return {
-         
         restrict: 'E',
         scope: {"image": '='},
-        compile: function(element,attributes)
+        compile: function (element, attributes)
         {
             
-          
-            
-            var linkFunction = function($scope, element, attributes) {
-                
-                var text = "<a href='https://www.pinterest.com/pin/"+
-                        
+
+
+            var linkFunction = function ($scope, element, attributes) {
+              
+                if ($scope.$parent.$last === true) {
+                    $timeout(function () {
+                       
+                        $scope.$emit('imagesCompleted');
+                    });
+                }
+
+
+                var text = "<a href='https://www.pinterest.com/pin/" +
                         $scope.image.id
-                        
-                        +"' data-pin-do='embedPin'></a>"
-                
-                element.html(text); 
-            
+
+                        + "' data-pin-do='embedPin'></a>"
+
+                element.html(text);
+
             }
 
             return linkFunction;
-            
-            
+
+
         }
     };
 });
--- public_html/sections/browse-images/browse-images.ctl.js
+++ public_html/sections/browse-images/browse-images.ctl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('BrowseImagesController', function ($log, images,$window) {
+angular.module('app').controller('BrowseImagesController', function ($scope,$log, images, $window) {
 
     var vm = this;
     vm.images = images.data;
@@ -7,12 +7,29 @@ angular.module('app').controller('BrowseImagesController', function ($log, image
     vm.currentPage = 1;
     vm.totalImages = images.data.length;
     // console.log(images.length)
-   // $window.parsePinBtns();
+    // $window.parsePinBtns();
+
+
+    /**
+     * this event handler handles the completed
+     * load of image pins. The event originates 
+     * in the image-widget-drct.js file
+     */
+    $scope.$on('imagesCompleted', function (ev) {
+      
+      //this is the renamed pinterest provided build 
+      //function see the data-pin-build tag of the 
+      // pinit script in index.html
+      // that tag exposes the build function under the name
+      // that is the attribute value. the function is attached to the
+      // global window object
+       parsePinBtns();
+    });
 
     vm.pageChanged = function ()
     {
-        
-         
+
+
         for (var i = 0; i < vm.maxImages; i++)
         {
             vm.imageBuffer[i] = null;
@@ -27,13 +44,33 @@ angular.module('app').controller('BrowseImagesController', function ($log, image
             maxLoop = vm.maxImages;
         }
         var start = (vm.currentPage - 1) * vm.maxImages;
-        
+
         for (var i = start; i < start + maxLoop; i++)
         {
             if (i < vm.images.length)
             {
-                vm.imageBuffer[i-start] = vm.images[i];
+                vm.imageBuffer[i - start] = vm.images[i];
             }
+        }
+        
+        //check if nulls exist and cut the array to non-null
+        var nullIdx = -1;
+        var delCount = 0;
+        for (var i = 0; i < vm.maxImages; i++)
+        {
+             if (vm.imageBuffer[i]== null)
+             {
+                 if (nullIdx == -1)
+                 {
+                     nullIdx = i;
+                 }
+                 
+                 delCount = delCount +1;
+             }
+        }
+        if (nullIdx > -1)
+        {
+            vm.imageBuffer.splice(nullIdx,delCount);
         }
 
     }
