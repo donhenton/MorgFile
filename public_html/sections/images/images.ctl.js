2fca6d90a8640eaa78aa8c8894ed03826b0b3881
d <dhenton@localhost.com>
2015-10-26 13:33:47 -0500

    moving forward setting up for adding browsing images for a folder2

--- public_html/sections/images/images.ctl.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').controller('ImagesController', function (folder, DialogService, FolderService, $log) {
-
-    var vm = this;
-    vm.folderName = folder.name;
-   // $log.debug(vm.folderName)
-
-});
