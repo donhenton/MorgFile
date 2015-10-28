2abd6f31af1096ec33624e2448df7a960cf4adc2
d <dhenton@localhost.com>
2015-10-28 10:56:14 -0500

    working on folder contents

---
+++ public_html/sections/folder-contents/folder-contents.ctl.js
@@ -0,0 +1,9 @@
+angular.module('app').controller('FolderContentsController',
+        function (DialogService, FolderService, $log ) {
+            var vm = this;
+             
+
+        });
+
+
+
